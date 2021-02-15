cc972c912e103a29d6968c22a5bc540386b35de5
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-15 15:08:49 +0300

    Create serverkey.cs

---
+++ serverkey.cs
@@ -0,0 +1,33 @@
+start.box
+start.box(cloud.name)
+start.box(enteredKey)
+   InputSystemValues.system(key.name)
+    key.system!("input.key")
+        box.system!("input")
+   host.server( == Computer.acces("key")
+       box.system(f++.
+          PlayerMov.acceskey
+       system.key( == plus.inp
+       config.mov( == plus.acces
+          input.system
+       PlayerSystem( == Computer.key
+       KeySystem( == inp.!system
+               System.val( == key.cloud
+      input.system(key ==f++.
+      input.server(host ==f++.
+          host.name( == server.
+      isHost( == server.func
+           Movement.plus
+      isHost( == system.Player
+          Player.key
+            createserver.input("system.player")
+     HostName( == local;000.input
+          ( == port.input
+    HostName( == host.player
+         f++.key
+    InputHost( == playerName.input
+        isHost( == playerFunction.input
+        isHost( == createAServer(.input
+     
+   
+
