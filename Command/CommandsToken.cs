52af7880acdbf728da32991a640413461c427df5
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-15 15:13:57 +0300

    Create CommandsToken.cs

---
+++ Command/CommandsToken.cs
@@ -0,0 +1,12 @@
+start.box
+start.box
+start.style
+    Host.name == MyBox.style
+    Host.input == MyPlayer.style
+       box(-(f++,
+    PlayerInput == MyBox.player
+        MyBoxToken.host
+          MyBox.style
+    Host.myBox( == AmongUsServer
+    Player.input( == CreateAmongUsServer
+       
