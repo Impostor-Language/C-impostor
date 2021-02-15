1a04c202a0667129ac163f76655ee88a4a477b2f
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-15 14:58:28 +0300

    Create style.c

---
+++ style.c
@@ -0,0 +1,48 @@
+#include <data>
+#include <console>
+#include <name>
+   function="0110, 0738"
+     print("java")
+     print("java")
+        style
+    function=0110, 0728"
+      print("getSystemAcces")
+      print("getSystemAcces")
+        style.lang(1,028)
+   //Tokens: Attention: This Tokens are Installed Mython!//
+      function="0110, 0738"
+       codename="java"
+     style="</>"
+        command.enter(class.border)
+     command.enter(java.enter)
+         function("enter.acces")
+       class.token("init.console")
+          impostor.function("start.codes")
+    //Blocks: Entered Command Blocks, command and more!//
+      enter.init(block.code)
+        { print("java").command
+        { print("getSystemAcces").command
+            symbol (.)
+            class.border symbol (<.>)
+        getSystemBorder.code
+        const("enterName.javascript")
+         const("c++").java
+        get.system(assembly.float
+             blocks.java
+        get.system(symbol.float
+             computer.java({a(++
+      getComputerAcces.starsCommands++
+       
+
+       
+     
+ 
+  
+ 
+  
+     
+     
+        
+
+
+ 
