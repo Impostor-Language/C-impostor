baab4ff4be8440002954594f4385372a4c908a39
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-24 11:19:28 +0300

    Create BrawlStarsServers.cs

---
+++ BrawlServer/BrawlStarsServers.cs
@@ -0,0 +1,11 @@
+inbox.style
+inbox.box
+inbox.brawlstars
+    (-(Game( MyBox == BrawlStars
+  public float; hostinfo{ = system.box
+      connect([ {your.ip
+  public skeld; BrawlStars.{ Server
+          setServerVar="BrawlStarsHost"
+          var.skeld(brawl
+  public/ float myGame.info
+          float.style( = { box.brawl
